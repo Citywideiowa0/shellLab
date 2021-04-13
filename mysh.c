267d9c59dbefd5f660379300fe6a95f1ba25c46c
Citywideiowa0 <spicermi@grinnell.edu>
2021-04-12 21:48:29 -0500

    a bunch of stuff

--- mysh.c
+++ mysh.c
@@ -5,10 +5,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
+
+// +------------------------+---------------------------------------------
+// | Globals & Declarations |
+// +------------------------+
 
 // This is the maximum number of arguments your shell should handle for one command
 #define MAX_ARGS 128
+
+void printArr(char**, int);
+
+void forkFunction(char**, int);
+
+// +------+---------------------------------------------
+// | Main |
+// +------+
 
 int main(int argc, char** argv) {
   // If there was a command line option passed in, use that file instead of stdin
@@ -49,9 +62,60 @@ int main(int argc, char** argv) {
       }
     }
 
-    // TODO: Execute the command instead of printing it below
-    printf("Received command: %s\n", line);
-  }
+    // ---------------  OUR CODE ---------------------------
+
+    // -------- Split into Command Modules --------
+    char* command = line;
+    while (true) {
+      // Call strpbrk to find the next occurrence of a delimeter
+      char* delim_position = strpbrk(command, ";");
+
+      if (delim_position == NULL) {
+        // There were no more delimeters.
+        if (strcmp(command, "") == 0) {
+          break;
+        }
+      } else {
+        // There was a delimeter. First, save it.
+        // char delim = *delim_position;
+
+        // Overwrite the delimeter with a null terminator so we can print just this fragment
+        *delim_position = '\0';
+
+        // printf("The fragment %s was found, followed by '%c'.\n", command, delim);
+      }
+
+      // --------- Split Cmmd Module into Arguments ----------
+
+      // Create an empty array of char*'s
+      char** myArgs = (char**)malloc(sizeof(char*) * MAX_ARGS);
+      // 'size' tracks the number of tokens from the input
+      int size = 0;
+      char* arg = strsep(&command, " \n");
+      while (arg != NULL) {
+        if (arg[0] != '\0') { /* excluding empty commands/arguments */
+          myArgs[size++] = arg;
+        }
+        arg = strsep(&command, " \n");
+      }
+      myArgs[size] = NULL;  // execvp requires NULL at the end of the array
+
+      if (strcmp(myArgs[0], "cd") == 0) {
+        if (chdir(myArgs[1]) != 0) {
+          perror("Changing directory not possible \n");
+          exit(1);
+        }
+      } else if (strcmp(myArgs[0], "exit") == 0) {
+        exit(0);
+      } else if (strcmp(myArgs[0], "") != 0) {
+        forkFunction(myArgs, size);
+      }
+
+      // Move our current position in the string to one character past the delimeter
+      if (delim_position == NULL) break;
+      command = delim_position + 1;
+    }  // end command module while loop
+  }    // end Professor's read input while loop
 
   // If we read in at least one line, free this space
   if (line != NULL) {
@@ -59,4 +123,60 @@ int main(int argc, char** argv) {
   }
 
   return 0;
+}
+
+// +--------------------+---------------------------------------------
+// | Helper Procedures  |
+// +--------------------+
+
+void printArr(char** arr, int len) {
+  for (int i = 0; i < len; i++) {
+    printf("args[%d]=[%s]\n", i, arr[i]);
+  }
+}
+
+void forkFunction(char** myArgs, int len) {
+  // MAKE FORKING A FUNCTION
+  // check for exit and cd (special cases), and maybe blank line
+  int id = fork();
+
+  // if fork failed
+  if (id < 0) {
+    perror("fork() failed");
+    exit(1);
+
+    // if child process
+  } else if (id == 0) {
+    // the p means to use the version of exec that finds the file using a path
+    //   configuration (basically searches for the command in all the configurred paths)
+    // 'bin' is conventional name for directory holding executable
+
+    // char **newArgs[] = {"ls","ls",NULL};
+    execvp(myArgs[0], myArgs);
+    perror("exec failure");
+    exit(EXIT_FAILURE);
+
+    // if parent
+  } else {
+    int status;
+    // Note: rc stands for return code; can also just use if statement
+    //  wait doesn't return 0 but the id of the child, so not equal
+    //    to zero or child's id
+    int rc_wait = wait(&status);
+    rc_wait = 1;
+    /*
+    printf("We're here\n");
+    int pid;
+    while ((pid = waitpid(id, &status, WNOHANG)) != 0) {
+      printf("[background process %d exited with status %d]", pid, WEXITSTATUS(status));
+    }
+    printf("Do we get here?\n");
+    */
+
+    // Note:
+    //   waitpid() wait for a specific process
+    //   wNoHang flag
+
+    printf("[%s exited with status %d]\n", myArgs[0], status);
+  }
 }
